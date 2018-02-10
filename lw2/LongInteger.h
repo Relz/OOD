96c881f9864d391a3fa71811a62f1738df9aac43
Alexey Starovoitov <alexey.starovoitov@travelline.ru>
2018-02-10 16:46:55 +0300

    Инициализирован класс длинного целого числа Реализовано составление числа и его вывод в поток

---
+++ lw2/LongInteger.h
@@ -0,0 +1,41 @@
+#ifndef LW2_LONGINTEGER_H
+#define LW2_LONGINTEGER_H
+
+#include "Digit.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class LongInteger
+{
+public:
+	explicit LongInteger(vector<Digit> const& digits = { Digit::ZERO })
+		: m_digits(digits)
+	{
+	}
+
+	friend ostream& operator<<(ostream& os, LongInteger const& longInteger)
+	{
+		for (Digit digit : longInteger.m_digits)
+		{
+			os << DigitExtensions::ToCharacter(digit);
+		}
+		return os;
+	}
+
+	static LongInteger CreateFromString(string const& str)
+	{
+		vector<Digit> digits;
+		for (char ch : str)
+		{
+			digits.emplace_back(DigitExtensions::CreateFromCharacter(ch));
+		}
+		return std::move(LongInteger(digits));
+	}
+
+private:
+	vector<Digit> m_digits;
+};
+
+#endif //LW2_LONGINTEGER_H
--- lw2/main.cpp
+++ lw2/main.cpp
@@ -1,4 +1,22 @@
+#include "LongInteger.h"
+#include <fstream>
+#include <regex>
+
+using namespace std;
+
 int main()
 {
+	ifstream inputFile("input.txt");
+
+	string line;
+	while (getline(inputFile, line))
+	{
+		smatch matches;
+		regex_search(line, matches, regex(R"(([0-9]+)\s([-+*\/])\s([0-9]+))"));
+		LongInteger operand1 = LongInteger::CreateFromString(matches.str(1));
+		char operation = matches.str(2).at(0);
+		LongInteger operand2 = LongInteger::CreateFromString(matches.str(3));
+		cout << operand1 << " " << operation << " " << operand2 << "\n";
+	}
 	return 0;
 }
