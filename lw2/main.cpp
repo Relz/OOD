55cab8ef9972e10d1fb483ed6a8ce17ce89f0472
Alexey Starovoitov <alexey.starovoitov@travelline.ru>
2018-02-10 17:06:41 +0300

    Размечены функции для математических операций над длинными целыми

--- lw2/main.cpp
+++ lw2/main.cpp
@@ -1,8 +1,21 @@
 #include "LongInteger.h"
 #include <fstream>
 #include <regex>
+#include <unordered_map>
 
 using namespace std;
+
+LongInteger Accumulate(LongInteger const& a, LongInteger const& b);
+LongInteger Subtract(LongInteger const& a, LongInteger const& b);
+LongInteger Multiply(LongInteger const& a, LongInteger const& b);
+LongInteger Divide(LongInteger const& a, LongInteger const& b);
+
+unordered_map<char, function<LongInteger(LongInteger const&, LongInteger const&)>> operationFunction = {
+	{ '+', Accumulate },
+	{ '-', Subtract },
+	{ '*', Multiply },
+	{ '/', Divide }
+};
 
 int main()
 {
@@ -16,7 +29,35 @@ int main()
 		LongInteger operand1 = LongInteger::CreateFromString(matches.str(1));
 		char operation = matches.str(2).at(0);
 		LongInteger operand2 = LongInteger::CreateFromString(matches.str(3));
-		cout << operand1 << " " << operation << " " << operand2 << "\n";
+		cout << operand1 << " " << operation << " " << operand2 << " = " << operationFunction.at(operation)(operand1, operand2) << "\n";
 	}
-	return 0;
+	return EXIT_SUCCESS;
+}
+
+LongInteger Accumulate(LongInteger const& a, LongInteger const& b)
+{
+	LongInteger result = a;
+
+	return result;
+}
+
+LongInteger Subtract(LongInteger const& a, LongInteger const& b)
+{
+	LongInteger result = a;
+
+	return result;
+}
+
+LongInteger Multiply(LongInteger const& a, LongInteger const& b)
+{
+	LongInteger result = a;
+
+	return result;
+}
+
+LongInteger Divide(LongInteger const& a, LongInteger const& b)
+{
+	LongInteger result = a;
+
+	return result;
 }
